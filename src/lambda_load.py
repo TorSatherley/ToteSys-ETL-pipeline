4b3463d35b678043ccaa80759929e270e77de241
TorSatherley <torsatherley@hotmail.co.uk>
2025-03-19 10:01:00 +0000

    some tidying, docstring writing and refactoring of lambda load

--- src/lambda_load.py
+++ src/lambda_load.py
@@ -10,7 +10,10 @@ from src.lambda_transform_utils import return_s3_key
 
 
 def lambda_handler(event, context):
-    """moves parquet files from the"""
+    """
+    retrieves parquet files from the 'processed' S3 bucket, and then loads them
+    into the appropriate table in the data warehouse.
+    """
     try:
         # Retrieve credentials
         sm_client = boto3.client(service_name="secretsmanager", region_name="eu-west-2")
@@ -53,7 +56,6 @@ def lambda_handler(event, context):
             # Build Insert query
             df_columns = df.columns.tolist()
             df_columns = ", ".join(df_columns)
-            id_column = df_columns.split(",")[0].strip()
             df_placeholders = ", ".join(["%s"] * len(df.columns))
 
             insert_query = f"""
@@ -62,7 +64,6 @@ def lambda_handler(event, context):
             VALUES 
             ({df_placeholders})
             """
-            print(f"insert_query:{insert_query}")
 
             for _, row in df.iterrows():
                 row_dict = row.to_dict()
@@ -73,13 +74,13 @@ def lambda_handler(event, context):
         conn.close()
         end_time = time.time()
         execution_time = end_time - start_time
-        print(execution_time / 60)
         return {"message": "Successfully uploaded to data warehouse"}
     except Exception as e:
         return {"message": f"Error: {e}"}
 
 
 def load_connection_psycopg2(db_credentials):
+    ''' use psychopg to connect to the data warehouse. '''
     try:
         conn = psycopg2.connect(
             dbname=db_credentials["database"],
@@ -97,6 +98,15 @@ def load_connection_psycopg2(db_credentials):
 
 
 def dw_cleanup(db_credentials):
+    '''
+    reset the datawarehouse ready for the data to be reuploaded.
+    Future improvements for this ETL pipeline would include only
+    collecting new and updated data from the ToteSys database and
+    refactoring lambda transform to be able to deal with this data 
+    and finally refactoring lambda load so that it only needs to
+    update the data warehouse with new or updated data rather than
+    totally re-seeding it each time.
+    '''
     conn = load_connection_psycopg2(db_credentials)
     cursor = conn.cursor()
     cursor.execute("DELETE FROM fact_sales_order")
@@ -108,14 +118,3 @@ def dw_cleanup(db_credentials):
     cursor.execute("DELETE FROM dim_staff")
     conn.commit()
     return {"message": "Date Warehouse restored to default state."}
-
-
-print(
-    lambda_handler(
-        {
-            "datetime_string": "20250312_143803",
-            "SECRET_NAME": "data-warehouse-credentials",
-        },
-        {},
-    )
-)
